66a87622233f5c0ff6d20020d29af03ff96e3c94
MHamdanHariri20 <m.hamdanhariri5@gmail.com>
2024-09-13 10:43:10 +0700

    add text in links service

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -19,12 +19,12 @@ function Service(){
                         <h2>Layanan Kami</h2>
                     </div>
                     <div className="content-flex">
-                        <a className="card" target="_blank" href="https://api.whatsapp.com/send?phone=6281211113526&text=Halo!%20boleh%20minta%20informasi%20lebih%20lanjut%20tentang%20Housekeeping%3F">
+                        <a className="card" target="_blank" href="https://api.whatsapp.com/send?phone=6281211113526&text=Halo!%20boleh%20minta%20informasi%20lebih%20lanjut%20tentang%20layanan%20Housekeeping%3F">
                             <img src={iconCleaning} alt="" />
                             <h3>Housekeeping</h3>
                             <p>General Cleaning, Deep Cleaning, dan Toilet Cleaning</p>
                         </a>
-                        <a className="card" target="_blank" href="https://api.whatsapp.com/send?phone=6281211113526&text=Halo!%20boleh%20minta%20informasi%20lebih%20lanjut%20tentang%20Laundry%3F">
+                        <a className="card" target="_blank" href="https://api.whatsapp.com/send?phone=6281211113526&text=Halo!%20boleh%20minta%20informasi%20lebih%20lanjut%20tentang%20layanan%20Laundry%3F">
                             <img src={iconCouch} alt="" />
                             <h3>Laundry</h3>
                             <p>Cuci berbagai jenis kasur, sofa, kursi, karpet, dan perlengkapan bayi</p>
